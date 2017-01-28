a4c5db2c1eb0d68eac4999b21f67b1dce9fa0411
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-01-28 18:37:04 +0100

    Adds menu on DetailActivity

--- app/src/main/java/com/kersuzananthony/popularmovies/activities/DetailActivity.java
+++ app/src/main/java/com/kersuzananthony/popularmovies/activities/DetailActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.kersuzananthony.popularmovies.R;
 import com.kersuzananthony.popularmovies.models.Movie;
@@ -27,5 +30,23 @@ public class DetailActivity extends AppCompatActivity {
                 Log.d(TAG, mMovie.toString());
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.movie_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Intent startSettingsActivityIntent = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivityIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
